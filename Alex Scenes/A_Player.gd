e31ef49a54c1bc1edb40a3e6c2dc892424cdd8f9
Polka <0121do1!@notanemail.com>
2024-01-24 02:47:03 +0000

    Made my own Player Scene that works, adjusted the models to accommodate it.

--- Alex Scenes/A_Player.gd
+++ Alex Scenes/A_Player.gd
@@ -1,12 +1,20 @@
 extends VehicleBody3D
 
+enum model_enum  {CheeseWheel, toilet, RiggedDude, cardboard_box}
 var speed = 5000
 var turn_speed = 20
 var drive = 0
 var turn = 0
+@export var model: model_enum
+@export var activate_input: bool = false
 
 func _ready():
-	pass
+	if activate_input == false:
+		set_process_input(false)
+	var model_string = "res://Game Objects/"+ model_enum.keys()[model] +".tscn"
+	add_child(load(model_string).instantiate())
+	(get_child(-1).get_child(-1)).reparent(self) #Inelegant, but interal doesn't work.
+	#print(get_tree_string_pretty())
 
 func _physics_process(delta):
 		engine_force = drive * delta
