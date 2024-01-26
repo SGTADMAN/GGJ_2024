b4bd4cc0c068313431fe150d75ef3ea034b1f5e3
Adam Lutton <sgtadman@gmail.com>
2024-01-26 18:37:57 +0000

    Added a pit to the Arena. Fixed the wheels clipping through the floor.

---
+++ Game Objects/Arena_Pit.cs
@@ -0,0 +1,29 @@
+using Godot;
+using System;
+using System.Collections;
+using System.Threading.Tasks;
+
+public partial class Arena_Pit : Node3D
+{
+    [Export]
+    public MeshInstance3D pitLid { get; set; }
+    [Export]
+    public float loweringSpeed { get; set; } = 10f;
+
+
+    public void OnPitEntered(Node3D body)
+    {
+        //Decrease player health to 0
+        
+    }
+
+    public async void LowerLid(Node3D body)
+    {
+        while(pitLid.Position.Y > -2.5f)
+        {
+            pitLid.Position -= new Vector3(0,loweringSpeed*(float)GetPhysicsProcessDeltaTime(),0);
+            await Task.Delay(10);
+        }
+        pitLid.Position = new Vector3(0, -2.5f, 0);
+    }
+}
